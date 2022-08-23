10635a3f2d8e5e390487e8c89ae3652e8757b400
Valeria Zernina <darovanie@mail.ru>
2022-08-23 23:42:45 +0500

    models

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 // запуск на 3000 порту
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+// подключаемся к серверу mongo
+mongoose.connect("mongodb://localhost:27017/mestodb", {
+  useNewUrlParser: true,
+});
+
+app.get("", (req, res) => {
+  res.send("Hello");
+});
+
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на ${PORT}`);
+});
